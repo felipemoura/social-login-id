16e4594f4248e145727941dff432d9eb5cee3dc9
Felipe Moura <mourafelp@gmail.com>
2017-01-27 11:48:33 -0200

    read me update
    updated description of read

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ A documentação sobre APIs disponíveis pode ser consultada no seguinte site: h
 
 ## Heroku
 
+A aplicação pode ser acessada em: https://login-social.herokuapp.com
+
+## Implementação
+
+1. 2 Páginas de HTML:
+	* Página de Login por Redes Sociais
+	* Página de informação caso sucesso de Login
+
+2. Controle de sessão utilizando express-session
+
+3. Roteamento de páginas invalidas
+
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// init
+// init =============================================
 var express 	= require('express');
 var session 	= require('express-session');
 var bodyParser = require('body-parser');
 var curl 		= require('curlrequest');
 var app 		= express();
 
+//=============================================
 app.set('port', (process.env.PORT || 5000));
-
+//=============================================
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 // set up ejs for templating
 app.set('view engine', 'ejs');
-
+//=============================================
 
 app.use(session({
   cookieName: 'session',
@@ -25,22 +26,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-// variables
+// variables ==========================
 var page = "https://api-staging.socialidnow.com/v1/marketing/login/info?api_secret=";
 var secret = "24979348df8f970ce19849ad861e215afa8a582f6e2f28f99b549da585e12bfb";
 var middle = "&token=";
 var fields = "&fields=birthday,gender,name,location";
 
-// session
+// session ===========================
 var sess;
 
-// Home
+// Home ==============================
 app.get('/',function(req,res){
 	sess = req.session;
 	res.render('pages/index.ejs'); // load the index.ejs file
 });
 
-// Post login
+// Post login ========================
 app.post('/login', function(req, res) {
 	sess = req.session;
 	sess.token = req.body.token;
@@ -48,7 +49,7 @@ app.post('/login', function(req, res) {
 	res.send('done');
 });
 
-// Login page 
+// Login page ========================
 app.get('/login', function(req, res) {
 	sess = req.session;
 
@@ -60,7 +61,7 @@ app.get('/login', function(req, res) {
 });
 
 
-// INFO ABOUT THE USER
+// INFO ABOUT THE USER ===============
 app.get('/info', function(req,res) {
 	sess = req.session;
 
